5c651d238f79b196a3e141566d69be1564537728
osher-steel <oshersteel@gmail.com>
2024-04-23 12:57:31 -0400

    Fixed creat

--- src/filesys.c
+++ src/filesys.c
@@ -94,33 +94,15 @@ void mount_fat32(const char *imgPath) {
 
 // Getting FAT32 File info
 void getInfo(){
-    printf("BS_jmpBoot: %02X %02X %02X\n",BootBlock.BS_jmpBoot[0], BootBlock.BS_jmpBoot[1], BootBlock.BS_jmpBoot[2]);
-    printf("BS_OEMName: %.8s\n", BootBlock.BS_OEMName);
-    printf("BPB_BytsPerSec: %u\n", BootBlock.BPB_BytsPerSec);
-    printf("BPB_SecPerClus: %u\n", BootBlock.BPB_SecPerClus);
-    printf("BPB_RsvdSecCnt: %u\n", BootBlock.BPB_RsvdSecCnt);
-    printf("BPB_NumFATs: %u\n", BootBlock.BPB_NumFATs);
-    printf("BPB_RootEntCnt: %u\n", BootBlock.BPB_RootEntCnt);
-    printf("BPB_TotSec16: %u\n", BootBlock.BPB_TotSec16);
-    printf("BPB_Media: %02X\n", BootBlock.BPB_Media);
-    printf("BPB_FATSz16: %u\n", BootBlock.BPB_FATSz16);
-    printf("BPB_SecPerTrk: %u\n", BootBlock.BPB_SecPerTrk);
-    printf("BPB_NumHeads: %u\n", BootBlock.BPB_NumHeads);
-    printf("BPB_HiddSec: %u\n", BootBlock.BPB_HiddSec);
-    printf("BPB_TotSec32: %u\n", BootBlock.BPB_TotSec32);
-    printf("BPB_FATSz32: %u\n", BootBlock.BPB_FATSz32);
-    printf("BPB_ExtFlags: %u\n", BootBlock.BPB_ExtFlags);
-    printf("BPB_FSVer: %u\n", BootBlock.BPB_FSVer);
-    printf("BPB_RootClus: %u\n", BootBlock.BPB_RootClus);
-    printf("BPB_FSInfo: %u\n", BootBlock.BPB_FSInfo);
-    printf("BPB_BkBootSec: %u\n", BootBlock.BPB_BkBootSec);
-    printf("BPB_Reserved: %.12s\n", BootBlock.BPB_Reserved);
-    printf("BS_DrvNum: %u\n", BootBlock.BS_DrvNum);
-    printf("BS_Reserved1: %u\n", BootBlock.BS_Reserved1);
-    printf("BS_BootSig: %02X\n", BootBlock.BS_BootSig);
-    printf("BS_VolID: %u\n", BootBlock.BS_VolID);
-    printf("BS_VolLab: %.11s\n", BootBlock.BS_VolLab);
-    printf("BS_FilSysType: %.8s\n", BootBlock.BS_FilSysType);
+    uint32_t total_clusters = BootBlock.BPB_TotSec32 / BootBlock.BPB_SecPerClus; 
+    uint32_t image_size = BootBlock.BPB_TotSec32 * BootBlock.BPB_BytsPerSec;
+
+    printf("Bytes per sector: %u\n", BootBlock.BPB_BytsPerSec);
+    printf("Sectors per cluster: %u\n", BootBlock.BPB_SecPerClus);
+    printf("Root cluster: %u\n", BootBlock.BPB_RootClus);
+    printf("Total # of clusters in data region: %u\n", total_clusters);
+    printf("Number of entries in FAT: %u\n", BootBlock.BPB_FATSz32);
+    printf("Size of image (bytes): %u\n", image_size);
 }
 
 // Diplays terminal as [NAME_OF_IMAGE]/[PATH_IN_IMAGE]/>
@@ -347,10 +329,12 @@ void create_file(const char *filename) {
         return;
     }
 
+
     // Create the directory entry for the new file
     dentry_t new_file_entry;
     memset(&new_file_entry, 0, sizeof(dentry_t));
-    snprintf(new_file_entry.DIR_Name, 12, "%.11s", filename);
+
+    snprintf(new_file_entry.DIR_Name, 11, "%.11s", filename);
     new_file_entry.DIR_Attr = 0x00; // File attribute
     new_file_entry.DIR_FstClusHI = 0;
     new_file_entry.DIR_FstClusLO = 0;
@@ -362,6 +346,7 @@ void create_file(const char *filename) {
     if (!buffer) {
         return; // Error handling in the read_current_directory_cluster function
     }
+
 
     // Find the first available slot in the current directory cluster
     dentry_t *entry = NULL;
